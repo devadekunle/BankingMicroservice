05e9af5de0cd5c43bf6f92bdb913a50bc4614225
Adekunle Alugbin <alugbin96@gmail.com>
2020-05-14 03:37:34 +0100

    Injected RabbitMq Bus

---
+++ BankingMicroservices.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using BankingMicroservice.Domain.Bus;
+using BankingMicroservice.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingMicroservices.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
